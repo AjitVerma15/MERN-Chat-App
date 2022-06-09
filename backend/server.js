df47f4671e84254481b0ac916497992f46f955de
AjitVerma15 <ajitverma1503@gmail.com>
2022-06-09 21:01:16 +0530

    Updated the url for socket.io

--- backend/server.js
+++ backend/server.js
@@ -45,7 +45,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://av-mern-chat-app.herokuapp.com/",
     // credentials: true,
   },
 });
