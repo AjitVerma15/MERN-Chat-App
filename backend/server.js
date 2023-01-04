af971eb8ded435f2e5ade7c7627fd7dd636e53ca
Ajit Verma <53833570+AjitVerma15@users.noreply.github.com>
2023-01-04 22:13:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 databaseConnection();
 const app = express();
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(express.json()); // to accept json data
 
 app.use("/api/user", userRoutes);
